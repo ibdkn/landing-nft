c55f4ac0a6f1055fbac13ecbc9811702fcbdf57a
ibdkn <ibudiakina@gmail.com>
2024-03-14 12:30:25 +0300

    added buttons for mobile and desktop

--- src/layout/section/unique/Unique.tsx
+++ src/layout/section/unique/Unique.tsx
@@ -8,6 +8,7 @@ import cardImage1 from "../../../assets/images/cards/card-image-1.png"
 import cardImage2 from "../../../assets/images/cards/card-image-2.png"
 import cardImage3 from "../../../assets/images/cards/card-image-3.png"
 import { Container } from '../../../components/Container';
+import {theme} from "../../../styles/Theme";
 
 export const Unique = () => {
     return (
@@ -18,13 +19,18 @@ export const Unique = () => {
                         <span>Amazing</span> and Super
                         Unique Art of This <span>Week</span>
                     </SectionTitle>
-                    <Button btnType={"primary"}>See All</Button>
+                    <DesktopButtonWrapper>
+                        <Button btnType={"primary"}>See All</Button>
+                    </DesktopButtonWrapper>
                 </FlexWrapper>
                 <CardsList>
                     <Card title={"Cyberpunk Cocomo"} src={cardImage1} price={"490"}/>
                     <Card title={"Charge, Qi tiao yu"} src={cardImage2} price={"490"}/>
                     <Card title={"Surgeon, Josh Rife"} src={cardImage3} price={"490"}/>
                 </CardsList>
+                <MobileButtonWrapper>
+                    <Button btnType={"primary"}>See All</Button>
+                </MobileButtonWrapper>
             </Container>
         </StyledUnique>
     );
@@ -34,11 +40,38 @@ const StyledUnique = styled.section`
   ${SectionTitle} {
     max-width: 506px;
   }
+
+  ${Container} > ${FlexWrapper} {
+    @media ${theme.media.tablet} {
+      justify-content: center;
+    }
+  }
+
 `
 
 const CardsList = styled.ul`
   margin-top: 64px;
   display: flex;
   justify-content: center;
+  flex-wrap: wrap;
   gap: 30px;
+
+  @media ${theme.media.tablet} {
+    margin: 32px 0;
+  } 
+`
+
+const DesktopButtonWrapper = styled.div`
+    @media ${theme.media.tablet} {
+      display: none;
+    }
+`
+
+const MobileButtonWrapper = styled.div`
+  display: none;
+
+  @media ${theme.media.tablet} {
+    display: block;
+    text-align: center;
+  }
 `
