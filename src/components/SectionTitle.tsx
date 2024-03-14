3ad395c4e0b6aaa2b8fdae8dd17896540e8a6734
ibdkn <ibudiakina@gmail.com>
2024-03-14 12:32:51 +0300

    added adaptive

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,13 +1,17 @@
 import styled from "styled-components";
 import {theme} from "../styles/Theme";
+import {font} from "../styles/Common";
 
 export const SectionTitle = styled.h3`
-  font-family: ${theme.fonts.secondary};
-  font-weight: 500;
-  font-size: 48px;
+  ${font({family: '"Canela Trial", sans-serif', weight: 500, Fmax: 48, Fmin: 31})};
   line-height: 120%;
   
   span {
+    white-space: nowrap;
     color: ${theme.colors.accent};
+  }
+
+  @media ${theme.media.tablet} {
+    text-align: center;
   }
 `
